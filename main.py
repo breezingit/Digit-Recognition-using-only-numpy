e3a12c80c64340f5d233826eccb6e905c04ded46
breezingit <71215996+breezingit@users.noreply.github.com>
2021-05-31 15:29:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ data=pd.read_csv(r'C:\Users\Yash Priyadarshi\train.csv')
 data = np.array(data)
 m, n = data.shape
 
-data_dev = data[0:1000].T
-Y_dev = data_dev[0]
-X_dev = data_dev[1:n]
-X_dev = X_dev / 255.
+data_test = data[0:1000].T
+Y_test = data_test[0]
+X_test = data_test[1:n]
+X_test = X_test / 255.
 
 data_train = data[1000:m].T
 Y_train = data_train[0]
@@ -23,7 +23,7 @@ num_labels=10
 
 Theta1,bias1,Theta2,bias2=gd.gradient_descent(X_train, Y_train, 0.1, 600, num_labels,input_layer_size,hidden_layer_size,m)
 
-fn.predict(X_dev,3, Theta1, bias1, Theta2, bias2)
-fn.predict(X_dev,4, Theta1, bias1, Theta2, bias2)
-fn.predict(X_dev,5, Theta1, bias1, Theta2, bias2)
-fn.predict(X_dev,6, Theta1, bias1, Theta2, bias2)
+fn.predict(X_test,3, Theta1, bias1, Theta2, bias2)
+fn.predict(X_test,4, Theta1, bias1, Theta2, bias2)
+fn.predict(X_test,5, Theta1, bias1, Theta2, bias2)
+fn.predict(X_test,6, Theta1, bias1, Theta2, bias2)
